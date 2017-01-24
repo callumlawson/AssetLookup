b9175b0ff3848b93e737d28c5e498500f1f0f90e
Callum Lawson <lawsonathome@gmail.com>
2017-01-24 20:59:57 +0000

    First Commit.

---
+++ Assets/Editor/AssetIndexObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    [Serializable]
+    class AssetIndexObject : ScriptableObject
+    {
+        public Dictionary<string, string> AssetToPathIndex; 
+    }
+}
---
+++ Assets/Editor/EditorWindowSettings.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public static class ScriptableObjectUtilities
+    {
+        public static T GetScriptableObject<T>() where T : ScriptableObject
+        {
+            var assetPath = GetPath<T>();
+
+            var possibleAsset = AssetDatabase.LoadAssetAtPath(assetPath, typeof(T));
+            if (possibleAsset)
+            {
+                return possibleAsset as T;
+            }
+            return CreateSettingsAsset(assetPath, ScriptableObject.CreateInstance<T>());
+        }
+
+        private static T CreateSettingsAsset<T>(string assetPath, T assetSettings) where T : ScriptableObject
+        {
+            AssetDatabase.CreateAsset(assetSettings, assetPath);
+            AssetDatabase.SaveAssets();
+            return assetSettings;
+        }
+
+        public static void SaveSettings<T>(T settings) where T : ScriptableObject
+        {
+            EditorUtility.SetDirty(settings);
+            AssetDatabase.SaveAssets();
+        }
+
+        private static string GetPath<T>() where T : ScriptableObject
+        {
+            return "Assets/Editor/EditorSettings/" + typeof(T) + ".asset";
+        }
+    }
+}
